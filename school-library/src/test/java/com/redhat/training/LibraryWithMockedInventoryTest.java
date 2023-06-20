1ffa2ae973529b3aeb6b76d3990a919b7c1f6aa4
Kannav <student@workstation.lab.example.com>
2023-06-20 03:17:35 -0400

    added test cases

--- school-library/src/test/java/com/redhat/training/LibraryWithMockedInventoryTest.java
+++ school-library/src/test/java/com/redhat/training/LibraryWithMockedInventoryTest.java
@@ -23,5 +23,25 @@ public class LibraryWithMockedInventoryTest {
         library = new Library(inventory);
     }
 
-    // Add tests here...
+@Test
+public void checkingOutWithdrawsFromInventoryWhenBookIsAvailable()
+throws BookNotAvailableException {
+
+when(inventory.isBookAvailable("book1")).thenReturn(true);
+
+library.checkOut("student1", "book1");
+
+verify(inventory).withdraw("book1");
+}
+
+@Test
+public void checkingOutDoesNotWithdrawFromInventoryWhenBookIsUnavailable()
+	throws BookNotAvailableException {
+
+	when(inventory.isBookAvailable("book1")).thenReturn(false);
+	try {
+		library.checkOut("student1", "book1");
+	} catch(BookNotAvailableException e) {}
+	verify(inventory, times(0)).withdraw("book1");
+}
 }
